2de613bee8c034438c33ffb77a0336b745971dd5
Michael Kempf <88schadenfreude@gmail.com>
2015-09-12 17:57:06 -0700

    html content, styling, board.js

--- board.js
+++ board.js
@@ -1,1 +1,23 @@
-//holds board info
+function Board(){
+  function convertToArray(htmlCollection) {
+    var newCopy = Array.prototype.slice.call(htmlCollection);
+    return newCopy;
+  }
+  var board = this.board = [
+    [{},{},{}],
+    [{},{},{}],
+    [{},{},{}]
+  ];
+
+  var rows = convertToArray(document.getElementById("board").children)
+  rows.forEach(function(row, rowI){
+    var cols = convertToArray(row.children)
+    cols.forEach(function(cell, colI){
+      board[rowI][colI].htmlRef = cell;
+      board[rowI][colI].value = null;
+
+      // board[rowI][colI].htmlRef.textContent = "";
+    });
+  });
+
+}
