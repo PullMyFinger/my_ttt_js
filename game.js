ef3f203f2b89b14cef21c54ca359e809be9e9b6b
Michael Kempf <88schadenfreude@gmail.com>
2015-09-12 18:44:47 -0700

    finished initialzing of a new game

--- game.js
+++ game.js
@@ -1,6 +1,25 @@
-//initialize board
-//get input for players
-//initialize players
+function Game(){
+  //initialize board
+  this.board = new Board();
+  //get input for players
+  var p1isHuman = this.isPlayerHuman();
+  var p2isHuman = this.isPlayerHuman();
+
+  //initialize players
+  this.p1 = new Player(p1isHuman, this.board)
+  this.p2 = new Player(p2isHuman, this.board)
+}
+
+Game.prototype.isPlayerHuman = function(){
+  var response;
+  do{
+    response = prompt("Is player human?(y/n)");
+  }while(response !== "y" && response !== "n");
+
+  return response === "y";
+}
+
+
 //start game loop.
   //p1 turn
   //check game over?
